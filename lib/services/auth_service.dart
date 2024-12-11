9d0c6aa4881a6de2a9f209eb57eb0ddfdc4d9ed5
Toka2104550 <125702984+Toka2104550@users.noreply.github.com>
2024-12-11 21:05:11 +0200

    login and sign up

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,1 +1,63 @@
+//Provides authentication functionality (sign in, sign up, sign out, etc.).
 import 'package:firebase_auth/firebase_auth.dart';
+//Allows interaction with Firestore (to read/write data in Firestore).
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/Admin.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  Future<User?> login(String email, String password) async {
+    try {
+      //userCredential: stores the result after Firebase tries to sign in the user
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<User?> signUp(String email, String password, String role) async {
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      User? user = userCredential.user;
+
+      if (user != null) {
+        await _firestore.collection('users').doc(user.uid).set({
+          'uid': user.uid,
+          'email': email,
+          'role': role, // Store role in Firestore
+          'createdAt': FieldValue.serverTimestamp(),
+        });
+      }
+      return user;
+    } catch (e) {
+      throw e;
+    }
+  }
+//method fetches a user’s information from Firestore by their uid
+  // Future<Admin?> getUserDetails(String uid) async {
+  //   try {
+  //     DocumentSnapshot doc =
+  //         await _firestore.collection('users').doc(uid).get();
+  //     if (doc.exists) {
+  //       return Admin.fromMap(doc.data() as Map<String, dynamic>);
+  //     }
+  //     return null;
+  //   } catch (e) {
+  //     throw e;
+  //   }
+  // }
+
+  // Future<void> logout() async {
+  //   await _auth.signOut();
+  // }
+}
