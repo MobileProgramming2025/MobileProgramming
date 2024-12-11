c848b77d76dbe306e44bc0410ba0092d325d7ce2
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2024-12-11 14:30:36 +0200

    connecting to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mobileprogramming/screens/Assignment/CreateAssignmentScreen.dart';
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
