201443c071dc37fe131723ce1e06aa15f4259c64
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2024-12-15 23:51:11 +0200

    User model

---
+++ lib/models/user.dart
@@ -0,0 +1,18 @@
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final String role;
+  final String department;
+  // final List<Course> courses;
+  
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.role,
+    required this.department,
+    // required this.courses,
+  });
+
+}
