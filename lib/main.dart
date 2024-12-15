00322b69187d4126674f7e2f0d841bed0b2c920f
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2024-12-15 21:20:22 +0200

    refactoring files

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mobileprogramming/screens/admin_home.dart';
 import 'package:mobileprogramming/screens/user_home.dart';
-import 'package:mobileprogramming/screens/signin.dart';
-import 'package:mobileprogramming/screens/signup.dart';
+import 'package:mobileprogramming/screens/Registration/signin.dart';
+import 'package:mobileprogramming/screens/Registration/signup.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
