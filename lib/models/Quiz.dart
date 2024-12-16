f2daf974a4b8e3d9761f2db91c020d13a5fa928c
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2024-12-16 22:40:56 +0200

    quizzes

--- lib/models/Quiz.dart
+++ lib/models/Quiz.dart
@@ -1,16 +1,28 @@
-
 import 'package:mobileprogramming/models/Question.dart';
 
 class Quiz {
   final String id;
   final String title;
-  final List<Question> questions;
   final int duration; 
+  final String courseId;
+  final List<Question> questions;
 
   Quiz({
     required this.id,
     required this.title,
+    required this.duration,
+    required this.courseId,
     required this.questions,
-    this.duration = 0, 
   });
+
+  // Convert to Map for Firestore
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'duration': duration,
+      'courseId': courseId,
+      'questions': questions.map((q) => q.toMap()).toList(),
+    };
+  }
 }
