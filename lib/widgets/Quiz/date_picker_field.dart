9cb14599b80bbda0a37be0722a4c6f7de85fd427
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2024-12-20 16:17:17 +0200

    textcontroller

--- lib/widgets/Quiz/date_picker_field.dart
+++ lib/widgets/Quiz/date_picker_field.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class DatePickerField extends StatelessWidget {
+class DatePickerField extends StatefulWidget {
   final String label;
   final DateTime initialDateTime;
   final ValueChanged<DateTime> onDateTimeChanged;
@@ -13,10 +13,31 @@ class DatePickerField extends StatelessWidget {
     required this.onDateTimeChanged,
   });
 
+  @override
+  _DatePickerFieldState createState() => _DatePickerFieldState();
+}
+
+class _DatePickerFieldState extends State<DatePickerField> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(
+      text: DateFormat('yyyy-MM-dd HH:mm').format(widget.initialDateTime),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   Future<void> _selectDateTime(BuildContext context) async {
     final DateTime? pickedDate = await showDatePicker(
       context: context,
-      initialDate: initialDateTime,
+      initialDate: widget.initialDateTime,
       firstDate: DateTime.now(),
       lastDate: DateTime(2100),
     );
@@ -25,20 +46,21 @@ class DatePickerField extends StatelessWidget {
 
     final TimeOfDay? pickedTime = await showTimePicker(
       context: context,
-      initialTime: TimeOfDay.fromDateTime(initialDateTime),
+      initialTime: TimeOfDay.fromDateTime(widget.initialDateTime),
     );
 
     if (pickedTime == null) return;
 
-    onDateTimeChanged(
-      DateTime(
-        pickedDate.year,
-        pickedDate.month,
-        pickedDate.day,
-        pickedTime.hour,
-        pickedTime.minute,
-      ),
+    final DateTime selectedDateTime = DateTime(
+      pickedDate.year,
+      pickedDate.month,
+      pickedDate.day,
+      pickedTime.hour,
+      pickedTime.minute,
     );
+
+    _controller.text = DateFormat('yyyy-MM-dd HH:mm').format(selectedDateTime);
+    widget.onDateTimeChanged(selectedDateTime);
   }
 
   @override
@@ -46,10 +68,10 @@ class DatePickerField extends StatelessWidget {
     return GestureDetector(
       onTap: () => _selectDateTime(context),
       child: TextField(
+        controller: _controller,
         enabled: false,
         decoration: InputDecoration(
-          labelText: label,
-          hintText: DateFormat('yyyy-MM-dd HH:mm').format(initialDateTime),
+          labelText: widget.label,
         ),
       ),
     );
