1af8ac0cc137a7cba64a041c991b8e3e0982e3c5
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2024-12-24 15:39:13 +0200

    Add User interface

--- lib/screens/AdminScreens/add_users.dart
+++ lib/screens/AdminScreens/add_users.dart
@@ -9,10 +9,51 @@ class AddUserScreen extends StatefulWidget {
 }
 
 class _AddUserScreenState extends State<AddUserScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _roleController = TextEditingController();
+  final TextEditingController _departmentController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add User'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(labelText: 'Name'),
+              ),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'E-mail'),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TextFormField(
+                controller: _roleController,
+                decoration: InputDecoration(labelText: 'Role'),
+              ),
+              TextFormField(
+                controller: _departmentController,
+                decoration: InputDecoration(labelText: 'Department'),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: null,
+                  child: Text('Add User')
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
