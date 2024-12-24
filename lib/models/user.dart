bbda938407d7cb7b423adb988b553ab3ac3c0348
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2024-12-24 15:00:09 +0200

    User object from and to a map for Firestore

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -15,4 +15,26 @@ class User {
     // required this.courses,
   });
 
+  // Convert a User object to a Map for Firestore
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'role': role,
+      'department': department,
+    };
+  }
+
+  // Create a User object from Firestore Map
+  static User fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['id'] as String,
+      name: map['name'] as String,
+      email: map['email'] as String,
+      role: map['role'] as String,
+      department: map['department'] as String,
+    );
+  }
+
 }
