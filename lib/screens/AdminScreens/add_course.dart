92fd742ee63ec31dc7f47364c7ce154fdf9e1a75
SaraYasser-03 <108815286+SaraYasser-03@users.noreply.github.com>
2024-12-27 20:24:19 +0200

    app icon

---
+++ lib/screens/AdminScreens/add_course.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddCourse extends StatelessWidget{
+  const AddCourse({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    throw UnimplementedError();
+  }
+
+}
