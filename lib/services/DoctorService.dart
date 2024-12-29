85444b778b952ef80731325fc225890757ed1540
Toka2104550 <125702984+Toka2104550@users.noreply.github.com>
2024-12-29 18:26:41 +0200

    ..

--- lib/services/DoctorService.dart
+++ lib/services/DoctorService.dart
@@ -26,6 +26,7 @@ class DoctorService {
 //list  of doctors as there maybe more than one doctor and map will include the details of the dr as name,email,..
   Stream<List<Map<String, dynamic>>> fetchDoctors() {
     //.snapshots(): creates a stream ashan lama ay update (any crud of dr in firestore) ysamaa ala tol
+    //.map() is used to transform the raw snapshot data into map that includes all dr details
     return _firestore.collection('doctors').snapshots().map((snapshot) {
       return snapshot.docs.map((doc) {
         return {
