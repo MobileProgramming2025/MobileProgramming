b2ee08997b63670eb60f8f80bda9920892083b1e
Toka2104550 <125702984+Toka2104550@users.noreply.github.com>
2024-12-29 18:19:38 +0200

    cc

--- lib/services/DoctorService.dart
+++ lib/services/DoctorService.dart
@@ -23,6 +23,7 @@ class DoctorService {
   }
 
 //Each data item in the stream is a list of maps. Each map represents a doctor’s details.
+//list  of doctors as there maybe more than one doctor and map will include the details of the dr as name,email,..
   Stream<List<Map<String, dynamic>>> fetchDoctors() {
     return _firestore.collection('doctors').snapshots().map((snapshot) {
       return snapshot.docs.map((doc) {
