3e687d650a8278767cf52a9cb76b9a2a4bbca648
SaraYasser-03 <108815286+SaraYasser-03@users.noreply.github.com>
2024-12-30 09:52:34 +0200

    delete course

--- lib/services/CourseService.dart
+++ lib/services/CourseService.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mobileprogramming/models/Course.dart';
+import 'package:mobileprogramming/models/user.dart';
+import 'package:mobileprogramming/services/user_service.dart';
 
 class CourseService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
+  final UserService _userService = UserService();
 
   Future<List<Course>> getAllCourses() async {
     final FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -13,6 +15,7 @@ class CourseService {
         .map((doc) => Course.fromMap(doc.data() as Map<String, dynamic>))
         .toList();
   }
+
   Future<List<Map<String, dynamic>>> getCourses() async {
     try {
       QuerySnapshot snapshot = await _firestore.collection('Courses').get();
@@ -33,9 +36,8 @@ class CourseService {
     }
   }
 
-
   // add course to Firestore
-  Future<void> addCourse({ 
+  Future<void> addCourse({
     required String id,
     required String name,
     required String code,
@@ -59,12 +61,44 @@ class CourseService {
     }
   }
 
-
   Future<void> deleteCourse(String id) async {
     try {
+      // Delete subcollections first (if any)
+      print("here ${id}");
+      
+      deleteAllCoursesFromUser(id);
+      
+      // Delete the parent document
       await _firestore.collection('Courses').doc(id).delete();
     } catch (e) {
+      print("Failed to delete Course: $e");
       throw Exception("Failed to delete Course: $e");
+    }
+  }
+
+  Future<void> deleteCourseFromUser(String userId, String courseId) async {
+    try {
+      print("courseId ${courseId}");
+      await _firestore
+          .collection('users')
+          .doc(userId)
+          .collection("enrolledCourses")
+          .doc(courseId)
+          .delete();
+    } catch (e) {
+      print("Failed to delete course: $e");
+      throw Exception("Failed to delete course");
+    }
+  }
+
+  Future<void> deleteAllCoursesFromUser(String courseId) async {
+    final users = await _userService.getAllUsers();
+    for (var user in users) {
+      for (var course in user.enrolledCourses) {
+        if (course.id == courseId) {
+          deleteCourseFromUser(user.id, courseId);
+        }
+      }
     }
   }
 }
