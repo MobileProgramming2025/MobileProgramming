14b895edce998c3b4269426e6c955a9a17e22fd6
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2025-01-16 17:02:52 +0200

    styling

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,13 @@ import 'package:mobileprogramming/screens/AdminScreens/view_department_courses.d
 import 'package:mobileprogramming/screens/AdminScreens/view_departments_screen.dart';
 import 'package:mobileprogramming/screens/AssignmentScreens/assignment_list_screen.dart';
 import 'package:mobileprogramming/screens/AssignmentScreens/student_assignment_list.dart';
-import 'package:mobileprogramming/screens/AssignmentScreens/ui_create_assignment.dart';
-import 'package:mobileprogramming/screens/CourseDesign/home_design_course.dart';
-import 'package:mobileprogramming/screens/CourseDetailScreen.dart';
 import 'package:mobileprogramming/screens/Registration/signin.dart';
 import 'package:mobileprogramming/screens/Registration/signup.dart';
 import 'package:mobileprogramming/screens/UserScreens/user_home.dart';
 import 'package:mobileprogramming/screens/Assignment/assignment_screen.dart';
 
 import 'package:flutter_sizer/flutter_sizer.dart';
+
 
 class HexColor extends Color {
   HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
