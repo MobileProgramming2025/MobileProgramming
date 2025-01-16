c18b9a3696fc13d8bd498cc60b67b012620d2995
AsmaaaMaher <116633142+AsmaaaMaher@users.noreply.github.com>
2025-01-16 16:04:32 +0200

    styling dr screen

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,17 @@ import 'package:mobileprogramming/screens/UserScreens/user_home.dart';
 import 'package:mobileprogramming/screens/Assignment/assignment_screen.dart';
 
 import 'package:flutter_sizer/flutter_sizer.dart';
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+}
 
 void main() async {
   // WidgetsFlutterBinding.ensureInitialized();
