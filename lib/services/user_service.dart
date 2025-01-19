991c8df4dd6df87faff6eec0ebbc05d65caf508e
SaraYasser-03 <108815286+SaraYasser-03@users.noreply.github.com>
2025-01-19 02:04:29 +0200

    login error

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -203,24 +203,4 @@ class UserService {
       throw Exception('Failed to enroll instructor: $e');
     }
   }
-
-  // Stream<List<Map<String, dynamic>>> fetchEnrolledCoursesByUserId(
-  //     String userId) {
-  //   return FirebaseFirestore.instance
-  //       .collection('users')
-  //       .doc(userId)
-  //       .snapshots()
-  //       .map((snapshot) {
-  //     // Extract the list document's ID and fields, and create a usable map
-  //     return [
-  //       {
-  //         'id': snapshot.id,
-  //         'enrolled_courses': snapshot.data()?['enrolled_courses'],
-  //       }
-  //     ];
-  //   });
-  // }
-  
-  
-  
-  }
+}
