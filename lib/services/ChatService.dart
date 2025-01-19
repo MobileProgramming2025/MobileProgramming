87fa996310be121fda04ec8a4e892be627232ac2
SaraYasser-03 <108815286+SaraYasser-03@users.noreply.github.com>
2025-01-20 01:09:13 +0200

    fixing import

--- lib/services/ChatService.dart
+++ lib/services/ChatService.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:mobileprogramming/models/message.dart';
+import 'package:mobileprogramming/models/Message.dart';
 
 class ChatService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
