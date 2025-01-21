eb3beb45d7d53a6156ca73a5ef5ade24b6ec9d31
Nermien19 <nermeinyaekoub@gmail.com>
2025-01-21 12:39:23 +0200

    admin dashboard

--- lib/screens/AdminScreens/admin_dashboard.dart
+++ lib/screens/AdminScreens/admin_dashboard.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:mobileprogramming/models/user.dart';
 import 'package:mobileprogramming/screens/partials/adminDrawer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class AdminDashboard extends StatelessWidget {
+class AdminDashboard extends StatefulWidget {
   final User admin;
 
   const AdminDashboard({super.key, required this.admin});
+
+  @override
+  _AdminDashboardState createState() => _AdminDashboardState();
+}
+
+class _AdminDashboardState extends State<AdminDashboard> {
+  int _numAdmins = 0;
+  int _numStudents = 0;
+  int _numDoctors = 0;
+  DateTime _focusedDate = DateTime.now();
+  DateTime? _selectedDate;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchStatistics();
+  }
+
+  void _fetchStatistics() async {
+    final usersCollection = FirebaseFirestore.instance.collection('users');
+    final adminsQuery =
+        await usersCollection.where('role', isEqualTo: 'Admin').get();
+    final studentsQuery =
+        await usersCollection.where('role', isEqualTo: 'Student').get();
+    final doctorsQuery =
+        await usersCollection.where('role', isEqualTo: 'Doctor').get();
+
+    setState(() {
+      _numAdmins = adminsQuery.docs.length;
+      _numStudents = studentsQuery.docs.length;
+      _numDoctors = doctorsQuery.docs.length;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,12 +54,135 @@ class AdminDashboard extends StatelessWidget {
           ),
         ),
       ),
-      drawer: AdminDrawer(user: admin),
-      body: Center(
-        child: Text(
-          'Swipe from the left or tap the menu icon to open the drawer.',
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.bodyLarge,
+      drawer: AdminDrawer(user: widget.admin),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildStatisticCard('Admins', _numAdmins, Colors.indigo[800]!),
+                _buildStatisticCard('Students', _numStudents, Colors.indigo[800]!),
+                _buildStatisticCard('Doctors', _numDoctors, Colors.indigo[800]!),
+              ],
+            ),
+            SizedBox(height: 16),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).canvasColor,
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        blurRadius: 8,
+                        spreadRadius: 2,
+                      ),
+                    ],
+                  ),
+                  child: TableCalendar(
+                    firstDay: DateTime.utc(2000, 1, 1),
+                    lastDay: DateTime.utc(2100, 12, 31),
+                    focusedDay: _focusedDate,
+                    selectedDayPredicate: (day) => isSameDay(_selectedDate, day),
+                    onDaySelected: (selectedDay, focusedDay) {
+                      setState(() {
+                        _selectedDate = selectedDay;
+                        _focusedDate = focusedDay;
+                      });
+                    },
+                    headerStyle: HeaderStyle(
+                      formatButtonVisible: false,
+                      titleCentered: true,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+                      ),
+                      titleTextStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                      leftChevronIcon:
+                          Icon(Icons.chevron_left, color: Colors.white),
+                      rightChevronIcon:
+                          Icon(Icons.chevron_right, color: Colors.white),
+                    ),
+                    calendarStyle: CalendarStyle(
+                      todayDecoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 255, 255, 255),
+                        border: Border.all(
+                            color: Theme.of(context).colorScheme.primary,
+                            width: 2),
+                        shape: BoxShape.circle,
+                      ),
+                      selectedDecoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        shape: BoxShape.circle,
+                      ),
+                      defaultDecoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      weekendDecoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.grey.withOpacity(0.1),
+                      ),
+                      outsideDecoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      defaultTextStyle: TextStyle(
+                          color: Theme.of(context).colorScheme.onSurface),
+                      weekendTextStyle: TextStyle(color: Colors.red),
+                      todayTextStyle: TextStyle(
+                        color: const Color.fromARGB(255, 56, 3, 3),
+                        fontWeight: FontWeight.bold,
+                      ),
+                      selectedTextStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      outsideDaysVisible: false,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStatisticCard(String title, int count, Color color) {
+    return Card(
+      color: color.withOpacity(0.1),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: color,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              count.toString(),
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: color,
+              ),
+            ),
+          ],
         ),
       ),
     );
